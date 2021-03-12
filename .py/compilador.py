020fb5af51193f37d652a1541048b3737a7c808f
Kevin G. Tertuliano <robotnyte@gmail.com>
2021-03-12 14:13:49 -0300

    :construction: : compilador melhorado

--- .py/compilador.py
+++ .py/compilador.py
@@ -1,10 +1,25 @@
 import PyInstaller.__main__
+from shutil import rmtree, copyfile
+from os import system, getlogin
 
-PyInstaller.__main__.run([
-    'main.py',
-    '--onefile',
-    '--clean',
-    '-n MaidOS',
-    '--log-level=WARN',
-    '--add-data="faxina.py:."'
-])
+raiz = f'C:Users/{getlogin()}/Documents/git/repositorios/MaidOS/'
+def compilar():
+    PyInstaller.__main__.run([
+        'main.py',
+        '--onefile',
+        '--clean',
+        '-n MaidOS',
+        '--log-level=WARN',
+        '--add-data=faxina.py;.'
+    ])
+
+
+def organizar():
+    #rmtree('build')
+    #system('del /F /A " MaidOS.spec"')
+    system(f'move "{raiz}.py/dist/MaidOS.exe" "{raiz}"')
+    #rmtree('dist')
+
+
+#compilar()
+organizar()
