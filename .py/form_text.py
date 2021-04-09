1a47bdfe5ff83a7607f3a181fb4583edfd01acda
Kevin G. Tertuliano <robotnyte@gmail.com>
2021-04-09 19:22:37 -0300

    :pencil: mudada a intensidade das cores

--- .py/form_text.py
+++ .py/form_text.py
@@ -5,18 +5,18 @@ To edit the text color:
      txt = 'text'
     """
     if c == 'red':
-        return '\033[31m{}\033[m'.format(txt)
+        return '\033[91m{}\033[m'.format(txt)
     elif c == 'white':
         return '\033[97m{}\033[m'.format(txt)
     elif c == 'green':
-        return '\033[32m{}\033[m'.format(txt)
+        return '\033[92m{}\033[m'.format(txt)
     elif c == 'yellow':
-        return '\033[33m{}\033[m'.format(txt)
+        return '\033[93m{}\033[m'.format(txt)
     elif c == 'blue':
-        return '\033[34m{}\033[m'.format(txt)
+        return '\033[94m{}\033[m'.format(txt)
     elif c == 'cyan':
-        return '\033[36m{}\033[m'.format(txt)
+        return '\033[96m{}\033[m'.format(txt)
     elif c == 'grey':
-        return '\033[37m{}\033[m'.format(txt)
+        return '\033[97m{}\033[m'.format(txt)
     elif c == 'magenta':
-        return '\033[35m{}\033[m'.format(txt)
+        return '\033[95m{}\033[m'.format(txt)
