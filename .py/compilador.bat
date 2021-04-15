87170fcdf9ea2614714530222fba2c054e09af82
Godofcoffe <robotnyte@gmail.com>
2021-04-15 11:55:53 -0300

    exe x64 criado para testes

--- .py/compilador.bat
+++ .py/compilador.bat
@@ -1,6 +1,11 @@
-pyinstaller --noconfirm --log-level=WARN ^
-    --onefile  ^
-    --add-data="..README;." ^
-    --add-data="faxina.py;." ^
-    --add-data="form_text.py;." ^
-    --icon=..\ ^
+python -m PyInstaller --noconfirm --log-level=WARN ^
+--onefile ^
+-n MaidOS ^
+--add-data="../README.md;." ^
+--add-data="faxina.py;." ^
+--add-data="form_text.py;." ^
+--add-data="../LICENSE;." ^
+main.py
+
+@echo off
+pause
